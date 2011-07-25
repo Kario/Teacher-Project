719076ff0582e08679d3a8710a215ba98ac702de
Snuggs <rashaun.stovall@clash-media.com>
2011-07-25 12:37:51 -0700

    Initial Commit:   - Using rails 3.1 * Note major versioning difference *

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeacherProject::Application.config.secret_token = 'fb764d008a758e39a71aa7257be2b14f7489e1a93d79138f5f8d95aa26135fe3a55ab81471e79736603e22ed2976890fa1e4ef9cf56ed138b9aa43cfa1c31e6f'
